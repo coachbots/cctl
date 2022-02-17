7de426c11f7eb1f559abbed078d0f62f7d8c6fdc
Marko Vejnovic <contact@markovejnovic.com>
2022-02-16 19:16:04 -0600

    Added waiting in bot boot tests

--- tests/feature/api/bot_ctl/test_boot.py
+++ tests/feature/api/bot_ctl/test_boot.py
@@ -22,24 +22,28 @@ class TestBootBot(BotTestCase):
         """Tests whether a robot can boot up successfully."""
         target_bot = self.random_testing_bot
         bc.boot_bot(target_bot.identifier, True)
+        self.wait_until_bots_reachable([target_bot])
         self.assert_bot_power(target_bot, True)
 
     def test_boot_negative(self):
         """Tests whether a robot can boot down successfully."""
         target_bot = self.random_testing_bot
         bc.boot_bot(target_bot.identifier, False)
+        self.wait_until_bots_reachable([target_bot])
         self.assert_bot_power(target_bot, False)
 
     def test_boot_multiple_positive(self):
         """Tests whether multiple bots can boot up successfully."""
         bc.boot_bots((bot.identifier for bot in self.test_bots),
                      sum(1 for _ in self.test_bots) * [True])
+        self.wait_until_bots_reachable(self.test_bots)
         self.assert_bot_powers(self.test_bots,
                                sum(1 for _ in self.test_bots) * [True])
 
     def test_boot_multiple_negative(self):
         """Tests whether multiple bots can be booted down successfully."""
         bc.boot_bots((bot.identifier for bot in self.test_bots), False)
+        self.wait_until_bots_reachable(self.test_bots)
         self.assert_bot_powers(self.test_bots,
                                sum(1 for _ in self.test_bots) * [True])
 
@@ -47,7 +51,7 @@ class TestBootBot(BotTestCase):
         """Tests whether get_alives operates as expected."""
         target_bot = self.random_testing_bot
         bc.boot_bot(target_bot.identifier, True)
-
+        self.wait_until_bots_reachable([target_bot])
         self.assertEqual([target_bot], list(bc.get_alives()))
 
 
