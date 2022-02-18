a89f678f3d00b24485e22b9ca97ed0500818b550
Marko Vejnovic <contact@markovejnovic.com>
2022-02-18 14:47:28 -0600

    Wrote more testcases for the netutils package.

--- tests/feature/test_netutils.py
+++ tests/feature/test_netutils.py
@@ -15,16 +15,37 @@ from cctl import netutils
 
 
 class TestNetutils(unittest.TestCase):
+    EXPECTED_IP_ADDRESS = ('enp60s0', '192.168.1.2')
+    EXPECTED_BROADCAST_ADDRESS = ('enp60s0', '192.168.1.255')
+
     """TestCase for the netutils."""
+    def test_get_ip_address(self):
+        """Tests whether the IP address is returned as expected."""
+        self.assertEqual(TestNetutils.EXPECTED_IP_ADDRESS[1],
+                         netutils.get_ip_address(
+                             TestNetutils.EXPECTED_IP_ADDRESS[0]))
+
+    def test_get_broadcast_address(self):
+        """Tests whether the broadcast address is fetched correctly."""
+        self.assertEqual(TestNetutils.EXPECTED_BROADCAST_ADDRESS[1],
+                         netutils.get_broadcast_address(
+                             TestNetutils.EXPECTED_BROADCAST_ADDRESS[0]))
+
     def test_ping(self):
         """Tests whether ping works as expected."""
         self.assertEqual(0,
                          netutils.ping(netutils.get_ip_address(
                              configuration.get_server_interface())))
 
-    def test_host_is_reachable(self):
+    def test_host_is_reachable_positive(self):
         """Tests whether host_is_reachable works as expected."""
         self.assertTrue(netutils.host_is_reachable('localhost'))
+
+    def test_host_is_reachable_negative(self):
+        """Tests whether host_is_reachable returns unreachable for an
+        unreachable device.
+        """
+        self.assertFalse(netutils.host_is_reachable('192.0.2.1'))
 
     def test_read_remote_file(self):
         """Tests whether read_remote_file can read a remote file on
