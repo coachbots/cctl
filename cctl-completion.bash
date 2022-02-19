f1cf0349e07afcd5860e35aa5267d70463ad936c
Marko Vejnovic <contact@markovejnovic.com>
2022-02-18 19:40:37 -0600

    Fixed completion to include new features

--- cctl-completion.bash
+++ cctl-completion.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-SUPPORTED_COMMANDS=('on' 'off' 'blink' 'start' 'pause' 'upload' 'manage', 'cam')
-ID_ARG_COMMANDS=('on' 'off' 'blink')
+SUPPORTED_COMMANDS=('on' 'off' 'blink' 'start' 'pause' 'upload' 'manage' \
+                    'cam' 'fetch-logs')
+ID_ARG_COMMANDS=('on' 'off' 'blink' 'fetch-logs')
 
 function _is_flag()
 {
@@ -23,30 +24,39 @@ function _is_id_arg()
 
 function _cctl_completions()
 {
-    # If the user is asking for help, that should be the first thing we
-    # suggest.
-    if _is_flag; then
-        COMPREPLY+=('--help')
-    fi
-
-    if [ "${COMP_WORDS[1]}" == "cam" ]; then
-        COMPREPLY+=($(compgen -W "setup preview" ${COMP_WORDS[COMP_CWORD]}))
-    fi
-
-    # If we're working on the upload, let's try to suggest the flags.
-    if [ "${COMP_WORDS[1]}" == "upload" ]; then
-        if _is_flag; then
-            COMPREPLY+=('--operating-system');
-        # Otherwise, we can suggest the files.
-        else
-            COMPREPLY+=($(compgen -A file ${COMP_WORDS[COMP_CWORD]}))
+    if [ "${COMP_WORDS[1]}" == "fetch-logs" ]; then
+        if [ "${COMP_WORDS[COMP_CWORD - 1]}" == "--directory" ] || \
+           [ "${COMP_WORDS[COMP_CWORD - 1]}" == "-d" ]; then
+            COMPREPLY+=($(compgen -d))
         fi
-    # Else if we are writing an ID-arg command, let's suggest "all"
-    elif _is_id_arg && [ $COMP_CWORD -eq 2 ]; then
-        COMPREPLY+=('all')
-    # If we're still on the subcommand, let's suggest the subommand
+        COMPREPLY+=($(compgen -W "--legacy --directory" -- \
+            "${COMP_WORDS[COMP_CWORD]}" ))
+        COMPREPLY+=($(compgen -W "all" -- "${COMP_WORDS[COMP_CWORD]}" ))
+
+    elif [ "${COMP_WORDS[1]}" == "cam" ]; then
+        if [ "$COMP_CWORD" == "2" ]; then
+            COMPREPLY+=($(compgen -W "setup preview" -- \
+                "${COMP_WORDS[COMP_CWORD]}"))
+        fi
+
+    elif [ "${COMP_WORDS[1]}" == "on" ]; then
+        COMPREPLY+=($(compgen -W "all" -- "${COMP_WORDS[COMP_CWORD]}"))
+    
+    elif [ "${COMP_WORDS[1]}" == "off" ]; then
+        COMPREPLY+=($(compgen -W "all" -- "${COMP_WORDS[COMP_CWORD]}"))
+
+    elif [ "${COMP_WORDS[1]}" == "blink" ]; then
+        COMPREPLY+=($(compgen -W "all" -- "${COMP_WORDS[COMP_CWORD]}"))
+
+    elif [ "${COMP_WORDS[1]}" == "upload" ]; then
+        COMPREPLY+=($(compgen -f -- \
+            "${COMP_WORDS[COMP_CWORD]}"))
+        COMPREPLY+=($(compgen -W "--operating-system" -- \
+            "${COMP_WORDS[COMP_CWORD]}"))
+
+
     elif [ $COMP_CWORD -eq 1 ] && ! _is_flag; then
-        COMPREPLY+=($(compgen -W "${SUPPORTED_COMMANDS[*]} " ${COMP_WORDS[1]}))
+        COMPREPLY+=($(compgen -W "${SUPPORTED_COMMANDS[*]}" -- ${COMP_WORDS[1]}))
     fi
 }
 
