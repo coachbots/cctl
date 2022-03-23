036b95a0ab6060190b8300db1169be9bb522f41f
Billie Strong <strong@northwestern.edu>
2022-03-23 10:20:42 -0500

    fixed typo

--- src/cctl/api/charge_ctl.py
+++ src/cctl/api/charge_ctl.py
@@ -9,7 +9,7 @@ Parameters:
     power: if true, the charger will turn on; if false, the charger will turn off
 """
 def charge_rail_set(port, power):
-    s = serial.(port,115200)
+    s = serial(port,115200)
     if power:
         s.write('A')
     else:
