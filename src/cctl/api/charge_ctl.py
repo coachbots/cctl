4f18f04020e93142a2c26cf9f872b3b04c762fcb
Billie Strong <strong@northwestern.edu>
2022-03-24 10:59:37 -0500

    Got charge control working

--- src/cctl/api/charge_ctl.py
+++ src/cctl/api/charge_ctl.py
@@ -1,19 +1,33 @@
 """This module provides functions for controlling the coachbot charging rails"""
 
-from Serial import serial
+from serial import Serial
 
-"""Changes the state of the charging rail
 
-Parameters:
-    port: the serial port to which the charge controller is connected
-    power: if true, the charger will turn on; if false, the charger will turn off
-"""
-def charge_rail_set(port, power):
-    s = serial(port,115200)
-    if power:
-        s.write('A')
-    else:
-        s.write('D')
-    s.close()
-    
+
+class ChargeRail:
+    """Class representing the charging rail. Use this class for charging rail
+    operations.
+    """
+
+    def __init__(self, port):
+        """Constructs a ChargeRail.
+        Parameters:
+            port: the serial port to which the charge controller is connected
+        """
+
+        self.serial = Serial(port,115200)
+
+    def charge_rail_set(self, power):
+        """Changes the state of the charging rail
+
+        Parameters:
+            power: if true, the charger will turn on; if false, the charger will turn off
+        """
+        if power:
+            self.serial.write(b'A')
+        else:
+            self.serial.write(b'D')
+        
+
+
 
