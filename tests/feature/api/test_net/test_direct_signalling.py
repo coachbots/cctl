bae6f6810bbd4a29659527cac27b84f2ef0a4f92
Marko Vejnovic <contact@markovejnovic.com>
2022-03-30 10:16:35 -0500

    Improved direct signalling tests

--- tests/feature/api/test_net/test_direct_signalling.py
+++ tests/feature/api/test_net/test_direct_signalling.py
@@ -4,6 +4,7 @@ import os
 import tempfile
 import textwrap
 import sys
+from time import sleep
 
 sys.path.insert(0, os.path.abspath('./src'))
 
@@ -35,11 +36,11 @@ class TestNetwork(BotTestCase):
                     robot.delay()
         """
 
-        successful = False
+        on_success_called = False
 
         def on_success(status: NetStatus):
-            nonlocal successful
-            successful = True
+            nonlocal on_success_called
+            on_success_called = True
             self.assertTrue(NetStatus.SUCCESS, status)
 
         with tempfile.NamedTemporaryFile('w') as usr_file:
@@ -49,17 +50,17 @@ class TestNetwork(BotTestCase):
             upload_code(usr_file.name, False)
             set_user_code_running(True)
 
-            network = Network()
-            try:
-                network.user.direct_signal('testsig', target_bot, b'',
-                                           on_success=on_success)
-            except ZMQError as zmqerr:
-                self.fail(zmqerr)
-            finally:
-                network.tear_down()
+        network = Network()
+        try:
+            network.user.direct_signal('testsig', target_bot, b'',
+                                       on_success=on_success)
+            sleep(2)  # Wait for two seconds for on_success to fire.
+        except ZMQError as zmqerr:
+            self.fail(zmqerr)
+        finally:
+            network.tear_down()
 
-        if not successful:
-            self.fail('on_success was not called.')
+        self.assertTrue(on_success_called)
 
     def test_direct_signal_invalid_status(self):
         """Tests whether coachbots can be messaged directly and whether an
@@ -78,7 +79,11 @@ class TestNetwork(BotTestCase):
                     robot.delay()
         """
 
+        on_error_called = False
+
         def on_error(status: NetStatus):
+            nonlocal on_error_called
+            on_error_called = True
             self.assertTrue(NetStatus.INVALID_RESPONSE, status)
 
         with tempfile.NamedTemporaryFile('w') as usr_file:
@@ -88,14 +93,17 @@ class TestNetwork(BotTestCase):
             upload_code(usr_file.name, False)
             set_user_code_running(True)
 
-            network = Network()
-            try:
-                network.user.direct_signal('testsig', target_bot, b'',
-                                           on_error=on_error)
-            except ZMQError as zmqerr:
-                self.fail(zmqerr)
-            finally:
-                network.tear_down()
+        network = Network()
+        try:
+            network.user.direct_signal('testsig', target_bot, b'',
+                                       on_error=on_error)
+            sleep(2)
+        except ZMQError as zmqerr:
+            self.fail(zmqerr)
+        finally:
+            network.tear_down()
+
+        self.assertTrue(on_error_called)
 
     def test_direct_signal_timeout(self):
         """Tests whether an unreachable coachbot does not crash anything."""
@@ -103,14 +111,21 @@ class TestNetwork(BotTestCase):
         target_bot = self.random_testing_bot
         target_bot.boot(False)
 
+        on_error_called = False
+
         def on_error(status: NetStatus):
+            nonlocal on_error_called
+            on_error_called = True
             self.assertEqual(NetStatus.TIMEOUT, status)
 
         network = Network()
         try:
             network.user.direct_signal('testsig', target_bot, b'',
                                        on_error=on_error)
+            sleep(2)
         except ZMQError as zmqerr:
             self.fail(zmqerr)
         finally:
             network.tear_down()
+
+        self.assertTrue(on_error_called)
