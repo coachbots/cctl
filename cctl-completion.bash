28b8e9cbd954a8c936cc6dc879c056ffce25777b
Marko Vejnovic <contact@markovejnovic.com>
2022-05-20 18:19:12 -0500

    Fixed cctl upload autocompletion

--- cctl-completion.bash
+++ cctl-completion.bash
@@ -49,11 +49,20 @@ function _cctl_completions()
         COMPREPLY+=($(compgen -W "all" -- "${COMP_WORDS[COMP_CWORD]}"))
 
     elif [ "${COMP_WORDS[1]}" == "upload" ]; then
-        COMPREPLY+=($(compgen -f -- \
-            "${COMP_WORDS[COMP_CWORD]}"))
         COMPREPLY+=($(compgen -W "--operating-system" -- \
             "${COMP_WORDS[COMP_CWORD]}"))
+        COMPREPLY=($(compgen -o plusdirs -f -X '!*.py' \
+            -- "${COMP_WORDS[COMP_CWORD]}"))
 
+        if [ ${#COMPREPLY[@]} = 2 ]; then
+            [ -d "$COMPREPLY" ] && LASTCHAR=/
+            COMPREPLY=$(printf %q%s "$COMPREPLY" "$LASTCHAR")
+        else
+            for ((i=0; i < ${#COMPREPLY[@]}; i++)); do
+                [ -d "${COMPREPLY[$i]}" ] && COMPREPLY[$i]=${COMPREPLY[$i]}/
+            done
+        fi
+        [[ $COMPREPLY == */ ]] && compopt -o nospace
 
     elif [ $COMP_CWORD -eq 1 ] && ! _is_flag; then
         COMPREPLY+=($(compgen -W "${SUPPORTED_COMMANDS[*]}" -- ${COMP_WORDS[1]}))
